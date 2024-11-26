f35d095cff59a83a4ebfd91333e06001e759e7ff
SUJANC-17 <sujanc3306@gmail.com>
2024-11-26 20:36:54 +0530

    Thisis last git

--- video.js
+++ video.js
@@ -35,12 +35,22 @@ function vidi()
         {
             if(d=="THANEESH@")
             {
-                document.getElementById("disp").innerHTML+='<img src="tha.jpg" width="403.2" height="302.4" alt="Success Image">';
+                document.getElementById("disp").innerHTML+='<img src="sri.jpg" width="403.2" height="302.4" alt="Success Image">';
             }
             else{
                 alert("The entered password is wrong");
             }
         }
+        else if(c=="SRIDHAR")
+            {
+                if(d=="SRIDHAR@")
+                {
+                    document.getElementById("disp").innerHTML+='<img src="tha.jpg" width="403.2" height="302.4" alt="Success Image">';
+                }
+                else{
+                    alert("The entered password is wrong");
+                }
+            }
     else{
         alert("The entered username is wrong");
     }
